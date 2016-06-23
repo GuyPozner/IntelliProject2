9f163e42771bd585d50fd9dd7d96da55e854265d
Guy Pozner <guy.pozner@mail.huji.ac.il>
2016-06-23 13:54:20 +0300

    Deleted files

--- sumNosepokes.R
+++
@@ -1,6 +0,0 @@
-sumNosepokes <- function(windowNosepokes, AnimalName, Day, ModuleName){
-        a <- sum(windowNosepokes$AnimalName == AnimalName &
-                         windowNosepokes$Day == Day &
-                         windowNosepokes$ModuleName == ModuleName)
-        a
-}
--- sumSideErrors.R
+++
@@ -1,7 +0,0 @@
-sumSideErrors <- function(windowNosepokes, AnimalName, Day, ModuleName){
-        a <- sum(windowNosepokes$AnimalName == AnimalName &
-                         windowNosepokes$Day == Day &
-                         windowNosepokes$ModuleName == ModuleName &
-                         windowNosepokes$SideError == TRUE)
-        a
-}

b236b72b045a771fa4583ae2e07611bf40f2ba02
Guy Pozner <guy.pozner@mail.huji.ac.il>
2016-06-29 13:14:01 +0300

    Added reaction times and impulse error graphs

--- bindNosepokes.R
+++
@@ -1,4 +0,0 @@
-bindNosepokes <- function(visits1, nosepokes1, nosepokes2){
-        nosepokes2$VisitID <- nrow(visits1) + nosepokes2$VisitID
-        nos <- rbind(nosepokes1, nosepokes2)
-}
--- sumImpulseErrors.R
+++
@@ -1,7 +0,0 @@
-sumImpulseErrors <- function(windowNosepokes, AnimalName, Day, ModuleName){
-        a <- sum(windowNosepokes$AnimalName == AnimalName &
-                         windowNosepokes$Day == Day &
-                         windowNosepokes$ModuleName == ModuleName &
-                         windowNosepokes$ImpulseError == 1)
-        a
-}
